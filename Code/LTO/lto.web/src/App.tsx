2ca014cc47a7837134c7917669845e6355658e3a
Vincent Pantia <vincent.m.pantia@hotmail.com>
2023-05-14 16:09:00 +0800

    [u] Changes

--- Code/LTO/lto.web/src/App.tsx
+++ Code/LTO/lto.web/src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// Components
+import Layout from "./pages/Layout";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Layout />}/>
+          <Route path="patient" element={<Layout />}/>
+          <Route path="doctor" element={<Layout />}/>
+          <Route path="nurse" element={<Layout />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- Code/LTO/lto.web/src/index.tsx
+++ Code/LTO/lto.web/src/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// Components
+import App from './App';
+
+// Styles
+import './style.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
