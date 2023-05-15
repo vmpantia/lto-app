c1e9dc476ce6083ecdeabd8fb921b47aaa0b21c9
Vincent Pantia <vincent.m.pantia@hotmail.com>
2023-05-15 10:54:10 +0800

    [u] Changes

--- Code/LTO/lto.web/src/pages/Login.tsx
+++ Code/LTO/lto.web/src/pages/Login.tsx
@@ -41,47 +41,56 @@ const Login = () => {
                         navigate("/"); /* Redirect to Layout Page */
                     })
                     .catch(err => {
-                        setErrorMessage(err.response.data);
+                        if(err.response === undefined)
+                            setErrorMessage(err.message);
+                        else if (err.response.data !== undefined)
+                            setErrorMessage(err.data);
+                        else 
+                            setErrorMessage("Error in sending request for login.");
                     })
     }
     
     return (
-        <div className='flex justify-center items-center h-screen'>
-            <div className='inline-flex w-92'>
-                <img className='h-72 w-50' src={require('../assets/Icons/lto-icon.png')} alt='No LTO Icon found'></img>
-                <form className='w-92'
-                        onSubmit={onSubmitLogin}>
-                    <h1 className='text-3xl font-bold mb-3'>Login User</h1>
-                    
-                    {errorMessage && 
-                        <div className='text-xs text-white bg-red-400 px-3 py-2 mb-2 rounded'> 
-                            {errorMessage}
+        <>
+            <div className='flex justify-center items-center h-screen'>
+                <img className='h-80 w-80 m-10' 
+                        src={require('../assets/Icons/lto-icon.png')} 
+                        alt='No LTO Icon found'>
+                </img>
+                <div className='w-1/4'>
+                    <form
+                            onSubmit={onSubmitLogin}>
+                        <h1 className='text-3xl font-bold mb-3'>Login User</h1>
+                        
+                        {errorMessage && 
+                            <div className='text-xs text-white bg-red-400 px-3 py-2 mb-2 rounded'> 
+                                {errorMessage}
+                            </div>
+                        }
+
+                        <InputField type='text' 
+                                    label='Username or Email' 
+                                    placeholder='Enter your Username or Email' 
+                                    value={loginName} 
+                                    isDisabled={isLoading} 
+                                    onChangedHandler={(e) => setLoginName(e.target.value)} />
+
+                        <InputField type='password' 
+                                    label='Password' 
+                                    placeholder='Enter your Password' 
+                                    value={password} 
+                                    isDisabled={isLoading} 
+                                    onChangedHandler={(e) => setPassword(e.target.value)} />
+
+                        <div>
+                            <Button type='submit' text='Login' isDisabled={isLoading} />
+                            <p className='text-center text-sm font-medium my-2'>- or -</p>
+                            <Button type='submit' text='Create Account' isDisabled={isLoading} />
                         </div>
-                    }
-
-                    <InputField type='text' 
-                                label='Username or Email' 
-                                placeholder='Enter your Username or Email' 
-                                value={loginName} 
-                                isDisabled={isLoading} 
-                                onChangedHandler={(e) => setLoginName(e.target.value)} />
-
-                    <InputField type='password' 
-                                label='Password' 
-                                placeholder='Enter your Password' 
-                                value={password} 
-                                isDisabled={isLoading} 
-                                onChangedHandler={(e) => setPassword(e.target.value)} />
-
-                    <div>
-                        <Button type='submit' text='Login' isDisabled={isLoading} />
-                        <p className='text-center text-sm font-medium my-2'>- or -</p>
-                        <Button type='submit' text='Create Account' isDisabled={isLoading} />
-                    </div>
-                </form>
-            </div>    
-
-        </div>
+                    </form>
+                </div>
+            </div>
+        </>
     )
 }
 
